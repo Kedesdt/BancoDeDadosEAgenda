b6aea859bdfa11bb61b5e3b764e0e783e951f249
Kedesdt <kedesdt@gmail.com>
2021-10-10 19:25:56 -0300

    Melhoria no banco, implementação do login criação do index

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 from flask import Flask, render_template
+import os, datetime
+import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "db.db"))
+
 app = Flask('__name__')
+app.config['SECRET_KEY'] = 'pass'
+app.config['SQLALCHEMY_DATABASE_URI'] = database_file
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
